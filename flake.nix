f14cd4dc35297772a4e02560eb4bf000a4d0b781
Jaziel Amadiz <jaziel.amadiz@pm.me>
2024-09-06 23:18:24 -0400

    remove nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+    #nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/24.05";
     sops-nix = {
     url = "github:Mic92/sops-nix";
@@ -21,20 +21,20 @@
      };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, auto-cpufreq, sops-nix,... }@inputs:
+  outputs = { self, nixpkgs, home-manager, auto-cpufreq, sops-nix,... }@inputs:
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-      overlay-unstable = final: prev: {
+      #overlay-unstable = final: prev: {
       #unstable = nixpkgs-unstable.legacyPackages.${prev.system};
         # use this variant if unfree packages are needed:
-         unstable = import nixpkgs-unstable {
-           inherit system;
-           config.allowUnfree = true;
-         };
+       #  unstable = import nixpkgs-unstable {
+        #   inherit system;
+       #    config.allowUnfree = true;
+       #  };
 
-      };
+      #};
     in {
     
       nixosConfigurations = {
@@ -42,7 +42,7 @@
           inherit system;
           # specialArgs = {inherit inputs;};
           modules = [ 
-          ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
+         # ({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
             ./nixos/configuration.nix
             ./modules
             #./sops.nix
@@ -57,7 +57,7 @@
         inherit pkgs;
         extraSpecialArgs = { inherit overlay-unstable; };
 	modules = [
-	({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
+	#({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
 	./home/home.nix ];
 		};  	
 	};
