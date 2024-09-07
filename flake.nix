70992efa35d3fc83e89e56fac0df664916333e84
Jaziel Amadiz <jaziel.amadiz@pm.me>
2024-09-06 23:29:15 -0400

    update auto-cpufreq channel

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     };
     auto-cpufreq = {
     url = "github:AdnanHodzic/auto-cpufreq";
-    inputs.nixpkgs.follows = "nixpkgs-stable";
+    inputs.nixpkgs.follows = "nixpkgs";
     };
 #      impermanence.url = "github:nix-community/impermanence";
 
