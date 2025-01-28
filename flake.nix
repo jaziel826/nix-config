0be6b1cf13903187959766c74651904f8ddba62e
Jaziel Amadiz <jaziel.amadiz@pm.me>
2025-01-28 16:49:44 -0500

    updated solaar

--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
       #url = "https://flakehub.com/f/Svenum/Solaar-Flake/0.1.1.tar.gz"; # uncomment line for solaar version 1.1.13
       #url = "github:Svenum/Solaar-Flake/main"; # Uncomment line for latest unstable version
       inputs.nixpkgs.follows = "nixpkgs";
+      };
     #auto-cpufreq = {
     #url = "github:AdnanHodzic/auto-cpufreq";
     #inputs.nixpkgs.follows = "nixpkgs-stable";
