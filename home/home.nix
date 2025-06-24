d8ccad1e1ad86eae0ecfea9bd513294d14581a07
Jaziel Amadiz <jaziel.amadiz@pm.me>
2025-06-24 16:16:34 -0400

    update yazi

--- home/home.nix
+++ home/home.nix
@@ -173,12 +173,7 @@ xdg.userDirs = {
 	};
 programs.yazi = {
   enable = true;
-  settings = {
-    manager = {
-      sort_by = "natural";
-      show_hidden = true;
-      show_symlink = true;
-    };
+  };
 #  keymap = {
 #    prepend.manager.keymap = [
 #      {
